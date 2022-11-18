3440b603089590272675aa69b7822e5bf44dbed1
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2022-11-18 15:50:24 +0200

    Small adjustments to map. Test object including almost accurate data created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Map from './Components/Map'
+import Maps from './Components/Maps'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Map />
+          <Maps />
       </header>
     </div>
   );
---
+++ src/Components/Maps.tsx
@@ -0,0 +1,251 @@
+import { GoogleMap, useJsApiLoader, Polygon } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '800px',
+  height: '800px'
+};
+
+const center = {
+  lat: 61.763119,
+  lng: 23.071493
+};
+
+const testObj = [
+  {
+    id: 1,
+    name: "Tevaniemi",
+    year: "1995",
+    mapMaker: "Ari Anjala, Jukka Pohjonen",
+    scale: "1:10 000",
+    description: "Tevaniemessä rastin löytävät vain tosisuunnistajat",
+    coordinates: [
+      { lat: 61.839594, lng: 23.074075 },
+      { lat: 61.838541, lng: 23.120970 },
+      { lat: 61.856278, lng: 23.116159 },
+      { lat: 61.854902, lng: 23.050351 },
+      { lat: 61.847046, lng: 23.049840 },
+      { lat: 61.839594, lng: 23.074075 }
+    ]
+  },
+  {
+    id: 2,
+    name: "Vatula/Kirkkojärvi",
+    year: "2011",
+    mapMaker: "Ari Anjala, Marius Mazulis, Jukka Pohjonen, Esko Savonen Kiintorastikartta",
+    scale: "1:10 000 & 1: 15 000",
+    description: "Kartalla järjestetty mm. KLL 2010, ja KLL 2002",
+    coordinates: [
+      { lat: 61.6810072, lng: 22.9363632 },
+      { lat: 61.6803559, lng: 22.9887199 },
+      { lat: 61.7089184, lng: 23.0285454 },
+      { lat: 61.7208710, lng: 23.0118942 },
+      { lat: 61.7284324, lng: 22.9930115 },
+      { lat: 61.7294087, lng: 22.9609108 },
+      { lat: 61.7274576, lng: 22.9274368 },
+      { lat: 61.6823097, lng: 22.9083824 },
+      { lat: 61.6811700, lng: 22.9361916 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Ikaalisten keskusta",
+    year: "2012",
+    mapMaker: "Marius Mazulis ja Jukka Pohjonen",
+    scale: "1:10 000 & 1: 5 000",
+    description: "Useita sprinttikarttoja (mm. Keskusta, Toivolansaari) ja suunnistuskartta.",
+    coordinates: [
+      { lng: 23.0383301, lat: 61.7769359 },
+      { lng: 23.0558395, lat: 61.7569647 },
+      { lng: 23.1160927, lat: 61.7403934 },
+      { lng: 23.1344604, lat: 61.7486802 },
+      { lng: 23.0961800, lat: 61.7637856 },
+      { lng: 23.0431366, lat: 61.7803444 },
+      { lng: 23.0379868, lat: 61.7768548 }
+    ]
+  },
+  {
+    id: 4,
+    name: "Kylpylä ja Teiharju",
+    year: "2015-2016",
+    mapMaker: "Jukka Pohjonen, Ari Anjala, Arvo Paulin",
+    scale: "1:10 000 & 1: 4 000",
+    description: "Sprinttikartta kylpylästä. Suunnistuskartta Teiharjusta (mm. Viestiliigan finaali 2020)",
+    coordinates: [
+      { lng: 23.0153275, lat: 61.7696780 },
+      { lng: 23.0306053, lat: 61.7734931 },
+      { lng: 23.0376434, lat: 61.7829881 },
+      { lng: 23.0069160, lat: 61.8053438 },
+      { lng: 22.9784203, lat: 61.8064789 },
+      { lng: 22.9681206, lat: 61.8042086 },
+      { lng: 23.0148125, lat: 61.7698903 }
+    ]
+  },
+  {
+    id: 5,
+    name: "Jämi",
+    year: "2022",
+    mapMaker: "Jukka Pohjonen, Arvo Paulin, Esko Savonen, Jarmo Pikkarainen",
+    scale: "1:10 000 & 1: 15 000",
+    description: "Jämin alueen kartta. Jämillä järjestetään vuosittain Jämi Suunnistusmaraton.",
+    coordinates: [
+      { lng: 22.7992058, lat: 61.7479097 },
+      { lng: 22.7792931, lat: 61.7504720 },
+      { lng: 22.7528572, lat: 61.7196715 },
+      { lng: 22.6465988, lat: 61.7335721 },
+      { lng: 22.6769829, lat: 61.7872460 },
+      { lng: 22.8285599, lat: 61.7683368 }
+    ]
+  },
+  {
+    id: 6,
+    name: "Riitiala",
+    year: "2022",
+    mapMaker: "Topias Helminen",
+    scale: "1:10 000",
+    description: "Uusi kartta, jossa myös kiintorastit.",
+    coordinates: [
+      { lng: 22.9774761, lat: 61.8696704 },
+      { lng: 23.0045128, lat: 61.8851612 },
+      { lng: 22.9802227, lat: 61.8987446 },
+      { lng: 22.9797077, lat: 61.8950664 },
+      { lng: 22.9736137, lat: 61.8841099 },
+      { lng: 22.9664898, lat: 61.8726640 },
+      { lng: 22.9773903, lat: 61.8697917 }
+    ]
+  },
+  {
+    id: 7,
+    name: "Juhtimäki",
+    year: "2006",
+    mapMaker: "Ari Anjala",
+    scale: "1:10 000",
+    description: "Perinteistä ja vaativaa ikaalilaista maastoa",
+    coordinates: [
+      { lng: 23.4467125, lat: 61.8276393 },
+      { lng: 23.4562397, lat: 61.8274368 },
+      { lng: 23.4621620, lat: 61.8312041 },
+      { lng: 23.4780407, lat: 61.8345661 },
+      { lng: 23.4825897, lat: 61.8371581 },
+      { lng: 23.4733200, lat: 61.8464310 },
+      { lng: 23.4671402, lat: 61.8506414 },
+      { lng: 23.4448242, lat: 61.8504390 },
+      { lng: 23.4467125, lat: 61.8276393 }
+    ]
+  },
+  {
+    id: 8,
+    name: "Väinänvuori, Konsonvuori, Himoistenvuoret",
+    year: "2019",
+    mapMaker: "Ari Anjala",
+    scale: "1:10 000",
+    description: "Luhalahden hienot maastot. HUOM! Suurin osa alueesta harjoituskiellossa vuoden 2025 SM-viestiin asti. Tarkista harjoituskieltoalue Suunnistusliiton karttarekisteristä!",
+    coordinates: [
+      { lng: 23.3043194, lat: 61.7754414 },
+      { lng: 23.3101559, lat: 61.7713193 },
+      { lng: 23.3116579, lat: 61.7637897 },
+      { lng: 23.3214855, lat: 61.7635258 },
+      { lng: 23.3251333, lat: 61.7630590 },
+      { lng: 23.3270645, lat: 61.7624297 },
+      { lng: 23.3332872, lat: 61.7627922 },
+      { lng: 23.3440590, lat: 61.7660804 },
+      { lng: 23.3534575, lat: 61.7638480 },
+      { lng: 23.3597660, lat: 61.7637103 },
+      { lng: 23.3632851, lat: 61.7743852 },
+      { lng: 23.3553028, lat: 61.7840017 },
+      { lng: 23.3565903, lat: 61.7865169 },
+      { lng: 23.3506680, lat: 61.7921145 },
+      { lng: 23.3501530, lat: 61.7935340 },
+      { lng: 23.3456898, lat: 61.7954401 },
+      { lng: 23.3329010, lat: 61.7940612 },
+      { lng: 23.3359909, lat: 61.7839205 },
+      { lng: 23.3220863, lat: 61.7818920 },
+      { lng: 23.3100700, lat: 61.7802712 },
+      { lng: 23.3038902, lat: 61.7753613 }
+    ]
+  },
+  {
+    id: 9,
+    name: "Nisunperä",
+    year: "?",
+    mapMaker: "?",
+    scale: "1:10 000",
+    description: "Nisunperän kartta. Nisunperällä on järjestetty mm. vuoden 1998 FIN5-rastiviikon osakilpailu.",
+    coordinates: [
+      { lng: 23.3550453, lat: 61.7455872 },
+      { lng: 23.3911800, lat: 61.7456308 },
+      { lng: 23.3753014, lat: 61.7603723 },
+      { lng: 23.3683491, lat: 61.7614685 },
+      { lng: 23.3608818, lat: 61.7603723 },
+      { lng: 23.3536720, lat: 61.7541192 },
+      { lng: 23.3547878, lat: 61.7455901 }
+    ]
+  },
+  {
+    id: 10,
+    name: "Särkijärvi",
+    year: "2014",
+    mapMaker: "Ari Anjala",
+    scale: "1:10 000",
+    description: "Kartalla järjestettiin mm. vuoden 1998 FIN5-rastiviikon osakilpailu",
+    coordinates: [
+      { lng: 23.3121300, lat: 61.7533135 },
+      { lng: 23.3147049, lat: 61.7416969 },
+      { lng: 23.3471489, lat: 61.7399093 },
+      { lng: 23.3512688, lat: 61.7555063 },
+      { lng: 23.3117867, lat: 61.7534759 }
+    ]
+  }
+]
+
+
+/*   
+{ lng: 23.3121300, lat: 61.7533135 },
+{ lng: 23.3147049, lat: 61.7416969 },
+{ lng: 23.3471489, lat: 61.7399093 },
+{ lng: 23.3512688, lat: 61.7555063 },
+{ lng: 23.3117867, lat: 61.7534759 }
+
+{ lng: 23.0458832, lat: 61.7635427 },
+{ lng: 23.0790997, lat: 61.7627714 },
+{ lng: 23.0853224, lat: 61.7712956 },
+{ lng: 23.0661392, lat: 61.7750902 },
+{ lng: 23.0495310, lat: 61.7785597 },
+{ lng: 23.0444241, lat: 61.7799595 },
+{ lng: 23.0391884, lat: 61.7800606 },
+{ lng: 23.0367851, lat: 61.7751507 },
+{ lng: 23.0458403, lat: 61.7636062 },
+ */
+// Tevaniemen koordinaatit.
+const paths = [
+  { lat: 61.839594, lng: 23.074075 },
+  { lat: 61.838541, lng: 23.120970 },
+  { lat: 61.856278, lng: 23.116159 },
+  { lat: 61.854902, lng: 23.050351 },
+  { lat: 61.847046, lng: 23.049840 },
+  { lat: 61.839594, lng: 23.074075 }
+]
+
+const Maps = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_API_KEY!
+  })
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+    >
+      <div>
+        {testObj.map((item) => {
+          return (
+            <Polygon key={item.id} paths={item.coordinates} />
+          )
+        })}
+      </div>
+      <></>
+    </GoogleMap>
+  ) : <h1>An error occurred</h1>
+}
+
+export default Maps
